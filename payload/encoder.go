6a4a2ea037e46f57336a1472df229f17e0d4d60e
Eugene Zadyra <untrance@gmail.com>
2019-01-15 15:55:47 +0100

    Speedup encoding

--- payload/encoder.go
+++ payload/encoder.go
@@ -2,7 +2,6 @@ package payload
 
 import (
 	"bytes"
-	"encoding/binary"
 	"reflect"
 	"strconv"
 
@@ -16,7 +15,7 @@ type encoder struct {
 //Encode struct to byte array
 func Encode(request interface{}) []byte {
 	value := reflect.ValueOf(request)
-	buf := &bytes.Buffer{}
+	buf := bytes.NewBuffer(make([]byte, 0, 200))
 	encoder := &encoder{buf}
 	encoder.encode(value)
 	return buf.Bytes()
@@ -73,20 +72,22 @@ func (e *encoder) slice(value reflect.Value, tags *tags) {
 }
 
 func (e *encoder) array(value reflect.Value, tags *tags) {
-	e.write(tags.endianness, value.Interface())
+	for i := 0; i < value.Len(); i++ {
+		e.write(tags.endianness, value.Index(i))
+	}
 }
 
 func (e *encoder) string(value reflect.Value, tags *tags) {
-	v := value.Interface().(string)
+	v := value.String()
 	if tags.hex.nonEmpty() {
 		size, _ := strconv.Atoi(string(tags.hex))
 		typ := types[size]
 		addr, _ := strconv.ParseUint(v[2:], 16, size*8)
-		v := convertTo(addr, typ)
+		v := valueConvertTo(reflect.ValueOf(addr), typ)
 		e.write(tags.endianness, v)
 	} else {
 		e.dynamicLength(len(v), tags)
-		e.write(tags.endianness, []uint8(v))
+		e.write(tags.endianness, reflect.ValueOf([]uint8(v)))
 	}
 }
 
@@ -97,29 +98,18 @@ func (e *encoder) uint(value reflect.Value, tags *tags, bitmaskBytes *uint64) {
 		}
 		bitmaskBits := bitmaskBits(tags.bits)
 		pos := util.FirstBitPosition(bitmaskBits)
-		v := valueConvertTo(value, uint64Type).Interface().(uint64)
+		v := valueConvertTo(value, uint64Type).Uint()
 		v = ((v << pos) & bitmaskBits)
 		*bitmaskBytes = (*bitmaskBytes) | v
 		if tags.bitmask == "end" {
-			v := convertTo(*bitmaskBytes, value.Type())
+			v := valueConvertTo(reflect.ValueOf(*bitmaskBytes), value.Type())
 			e.write(tags.endianness, v)
 		}
 	} else if tags.bound.nonEmpty() {
 		size, _ := strconv.Atoi(string(tags.bound))
-		v := make([]uint8, size, size)
-		uintVal := value.Uint()
-		endianness := tags.endianness
-		for i := 0; i < size; i++ {
-			if endianness == "be" {
-				v[size-i-1] = byte(uintVal >> byte(i*8))
-			} else {
-				v[i] = byte(uintVal >> byte(i*8))
-			}
-		}
-		e.write(tags.endianness, v)
+		e.writeUint(value.Uint(), size, tags.endianness)
 	} else {
-		v := value.Interface()
-		e.write(tags.endianness, v)
+		e.write(tags.endianness, value)
 	}
 }
 
@@ -132,11 +122,37 @@ func (e *encoder) dynamicLength(length int, tags *tags) {
 		size, _ := strconv.Atoi(string(tags.size))
 		if typ, ok := types[size]; ok {
 			v := valueConvertTo(reflect.ValueOf(length), typ)
-			e.write(tags.endianness, v.Interface())
+			e.write(tags.endianness, v)
 		}
 	}
 }
 
-func (e *encoder) write(endianness tag, v interface{}) {
-	binary.Write(e.buf, order(endianness), v)
+func (e *encoder) write(endianness tag, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Uint8:
+		e.buf.WriteByte(uint8(v.Uint()))
+	case reflect.Uint16:
+		e.writeUint(v.Uint(), 2, endianness)
+	case reflect.Uint32:
+		e.writeUint(v.Uint(), 4, endianness)
+	case reflect.Uint64:
+		e.writeUint(v.Uint(), 8, endianness)
+	case reflect.Slice:
+		l := v.Len()
+		for i := 0; i < l; i++ {
+			e.write(endianness, v.Index(i))
+		}
+	}
+}
+
+func (e *encoder) writeUint(t uint64, size int, endianness tag) {
+	if endianness == "be" {
+		for i := 0; i < size; i++ {
+			e.buf.WriteByte(byte(t >> byte((size-i-1)*8)))
+		}
+	} else {
+		for i := 0; i < size; i++ {
+			e.buf.WriteByte(byte(t >> byte(i*8)))
+		}
+	}
 }
